9e520b413be9c2fe589aefbc435dca876fca1015
Marco Emrich <marco.emrich@codecentric.de>
2019-10-09 20:02:57 +0200

    cleanup

--- cypress/integration/tic_tac_toe.js
+++ cypress/integration/tic_tac_toe.js
@@ -5,8 +5,6 @@ context('Actions', () => {
     cy.visit('http://localhost:1234')
   })
 
-  // https://on.cypress.io/interacting-with-elements
-
   it('should Mark a Cell with X', () => {
     cy.get('h1').should("contain.text", "Tic Tac Toe")
   });
