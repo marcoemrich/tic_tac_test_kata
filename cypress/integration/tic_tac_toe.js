7a294c108f1b5eaf832406edc67b856954aae93e
Marco Emrich <marco.emrich@codecentric.de>
2020-02-03 23:47:29 +0100

    add cypress exercise file

--- cypress/integration/tic_tac_toe.js
+++ cypress/integration/tic_tac_toe.js
@@ -5,8 +5,17 @@ context('Actions', () => {
     cy.visit('http://localhost:1234')
   })
 
-  it('should Mark a Cell with X', () => {
+  it('should have a headline', () => {
     cy.get('h1').should("contain.text", "Tic Tac Toe")
   });
+
+  it('should start with an empy cell');
+  it('should start be marked with an X');
+  it('should mark with an O after 2nd Click');
+  it('should mark with an O after 2nd Click, in the third row');
+  it('should determine a winner');
+  it('allow to enter player names');
+  it('player names should be corrected to Uppercase');
+  it('player should be declared winner with a name');
 
 })
